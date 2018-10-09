633ff2bdaadf7c17adc3842b2840f334f3ccf765
alperalanyali <alper@alper-macbook-pro.local>
2018-10-09 11:33:00 +0300

    Initial Commit

---
+++ Insta Clone Firebase/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Insta Clone Firebase
+//
+//  Created by Alper on 9.10.2018.
+//  Copyright © 2018 Alper. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Insta Clone Firebase/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Insta Clone Firebase
+//
+//  Created by Alper on 9.10.2018.
+//  Copyright © 2018 Alper. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
